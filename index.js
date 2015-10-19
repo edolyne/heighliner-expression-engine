1c2757fffb9b5e30378819866de39139ac31b44b
James Baxley <james@whitefloordesign.com>
2015-10-18 21:44:25 -0400

    working mongo connection as well, ready to start building app

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 "use strict";
 
-
-const express = require("express"),
-      LiveSelect = require("mysql-live-select");
-
-// Constants
-const PORT = 8080;
-
-// App
-const app = express();
-app.get("/", function (req, res) {
-  res.send("Hello Tristan\n");
-});
-
-app.listen(PORT);
-console.log("Running on http://localhost:" + PORT);
+const LiveSelect = require("mysql-live-select"),
+      Mongoose = require('mongoose');
 
 
 /*
 
+  SQL
 
 */
 const SQLSettings = {
@@ -46,24 +34,16 @@ process.on("SIGTERM", closeAndExit);
 // Close connections on exit (ctrl + c)
 process.on("SIGINT", closeAndExit);
 
-const devotionals = liveDb.select(
-  "SELECT d.entry_id, d.site_id, d.channel_id, d.field_id_18, d.field_id_654, d.field_id_1028, t.title, t.url_title, t.status, t.year, t.month, t.day, t.author_id, m.m_field_id_2, m.m_field_id_3, m.m_field_id_4 " +
-  "FROM " +
-    "exp_channel_data AS d " +
-      "LEFT JOIN exp_channel_titles AS t " +
-        "ON d.entry_id = t.entry_id " +
-      "LEFT JOIN exp_member_data as m " +
-        "ON t.author_id = m.member_id " +
-  "WHERE d.channel_id=27 "
-  ,
-  [
-    { table: "exp_channel_data" },
-    { table: "exp_channel_titles" },
-    { table: "exp_channel_member_data" }
-  ]
-);
 
+/*
 
-devotionals.on("update", (results) => {
-  console.log(`Synced data for ${results.added.length} results`)
-});
+  Mongo
+
+*/
+Mongoose.connect("mongodb://192.168.99.100/test");
+
+const db = Mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function (callback) {
+  // yay!
+})
